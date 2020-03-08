5f9a21c7207d567179629019028364eff0cbd7a1
lostsquirrel <songli1988@gmail.com>
2020-03-08 16:12:40 +0800

    add http demos

---
+++ custom/custom.go
@@ -0,0 +1,27 @@
+package custom
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"time"
+)
+
+type FooHandler struct{}
+
+func (f FooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func CreateServe() {
+	myHandler := FooHandler{}
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        myHandler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
+}
---
+++ server.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"./custom"
+)
+
+
+
+func main() {
+	custom.CreateHandleServer()
+}
